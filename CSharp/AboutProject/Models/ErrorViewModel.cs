e856c9aff9d898f3ebc858acbd3eebad009aafa3
LENOVO <john.oduor.j@gmail.com>
2023-07-02 13:50:40 +0300

    About Page in a project

---
+++ CSharp/AboutProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AboutProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
